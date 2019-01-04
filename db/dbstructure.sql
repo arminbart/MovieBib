2190a1d365509973cd3a9586301fe310ef46aed5
Armin <Armin.Bart@xpecto.com>
2019-01-04 13:22:04 +0100

    New fields in table Videos

--- db/dbstructure.sql
+++ db/dbstructure.sql
@@ -6,16 +6,28 @@ CREATE TABLE Genres (
 
 CREATE TABLE Videos (
 	ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	Status INT(1),
+	Rating INT(1),
 	Title TEXT,
 	OrigTitle TEXT,
+	OtherTitles TEXT,
+	Predecessor INT UNSIGNED,
+	Successor INT UNSIGNED,
 	Enlisted TIMESTAMP,
+	Country VARCHAR(16),
 	Genre VARCHAR(16),
 	Lang CHAR(2),
 	Year INT(4),
+	Duration INT(4),
 	Medium VARCHAR(16),
 	Resolution VARCHAR(16),
+	Cut BOOLEAN NOT NULL DEFAULT FALSE,
+	File TEXT,
 	Location TEXT,
 	Info TEXT,
+	Link TEXT,
+	Director VARCHAR(64),
+	Actors VARCHAR(1024),
 	Watched TIMESTAMP,
 	WatchCnt INT
 )
