7b4846cfd701f677629152b248a896e3f822fefa
Armin <Armin.Bart@xpecto.com>
2019-01-04 23:51:54 +0100

    Redirect to show_video.php after uploading and resizing cover image

--- save_cover.php
+++ save_cover.php
@@ -17,55 +17,49 @@ else
 	move_uploaded_file($tmpname, $filename);
 
 	list($w, $h) = getimagesize($filename);
-	
+
 	$maxw = get_php_param("cover_max_width");
 	$maxh = get_php_param("cover_max_height");
-	echo $w . " x " . $h;
-	echo "<br>max: " . $maxw . " x " . $maxh;
 
-	if ($w <= $maxw and $h <= $maxh)
-	{ // Width and Height ok -> Smile :-)
-		header("Location: http://video.bartmail.de/show_video.php?id=" . $id);
-		exit();
-	}
-
-	$oldimg = imagecreatefromjpeg($filename);
-
-	if ($w > $maxw and $h > $maxh)
-	{ // Width and Height too large -> Zoom
-		$factorw = $w / $maxw;
-		$factorh = $h / $maxh;
-		echo "<br>factor: " . $factorw . " x " . $factorh;
-
-		$factor = $factorw < $factorh ? $factorh : $factorw; 
-		$neww = $w / $factor;
-		$newh = $h / $factor;
-
-		echo "<br>new: " . $neww . " x " . $newh . " -> " . $factor;
-
-		$newimg = imagecreatetruecolor($neww, $newh);
-		imagecopyresampled($newimg, $oldimg, 0, 0, 0, 0, $neww, $newh, $w, $h);
-		imagedestroy($oldimg);
-		$oldimg = $newimg;
-		$w = $neww;
-		$h = $newh;
-	}
-/*
 	if ($w > $maxw or $h > $maxh)
-	{ // Width or Height (still) too large -> Crop
-		$offsetx = $neww > $maxw ? ($neww - $maxw) / 2 : 0;
-		$offsety = $newh > $maxh ? ($newh - $maxh) / 2 : 0;
-		$newimg = imagecrop($oldimg, array('x' => $offsetx, 'y' => $offsety, 'width' => min($w, $maxw), 'height' => min($h, $maxh)));
-		imagedestroy($oldimg);
+	{
+		$oldimg = imagecreatefromjpeg($filename);
 
-		echo "<br>offset " . $offsetx . " x " . $offsety;
-		echo "<br>range " . min($neww, $maxw) . " x " . min($newh, $maxh);
-	}
+		if ($w > $maxw and $h > $maxh)
+		{ // Width and Height too large -> Zoom
+			$factorw = $w / $maxw;
+			$factorh = $h / $maxh;
+			$factor = $factorw < $factorh ? $factorh : $factorw;
+			$neww = $w / $factor;
+			$newh = $h / $factor;
+
+			$newimg = imagecreatetruecolor($neww, $newh);
+			imagecopyresampled($newimg, $oldimg, 0, 0, 0, 0, $neww, $newh, $w, $h);
+			imagedestroy($oldimg);
+			$oldimg = $newimg;
+			$w = $neww;
+			$h = $newh;
+		}
+/*
+		if ($w > $maxw or $h > $maxh)
+		{ // Width or Height (still) too large -> Crop
+			$offsetx = $neww > $maxw ? ($neww - $maxw) / 2 : 0;
+			$offsety = $newh > $maxh ? ($newh - $maxh) / 2 : 0;
+			$newimg = imagecrop($oldimg, array('x' => $offsetx, 'y' => $offsety, 'width' => min($w, $maxw), 'height' => min($h, $maxh)));
+			imagedestroy($oldimg);
+
+			echo "<br>offset " . $offsetx . " x " . $offsety;
+			echo "<br>range " . min($neww, $maxw) . " x " . min($newh, $maxh);
+		}
 */
-	imageconvolution($newimg, array(array(-1, -1, -1), array(-1, 16, -1), array(-1, -1, -1)), 8, 0); // Sharpen image
-	imagejpeg($newimg, $filename);
+		imageconvolution($newimg, array(array(-1, -1, -1), array(-1, 16, -1), array(-1, -1, -1)), 8, 0); // Sharpen image
+		imagejpeg($newimg, $filename);
 
-	imagedestroy($newimg);
+		imagedestroy($newimg);
+	}
+
+	header("Location: http://video.bartmail.de/show_video.php?id=" . $id);
+	exit();
 }
 
 ?>
