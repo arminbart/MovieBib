9488482fbac8de2b07eba420e640f9566042bc51
Armin <Armin.Bart@xpecto.com>
2019-01-07 14:05:22 +0100

    Cover image size 150x200 -> 225x300

--- show_video.php
+++ show_video.php
@@ -80,12 +80,12 @@
 						<td colspan="4" id="text_small"><?php echo $row["Info"] != "" ? $row["Info"] : "Keine Beschreibung vorhanden"; ?></td>
 					</tr>
 					<tr>
-						<td colspan="4" id="spacer_medium"></td>
+						<td colspan="4" id="spacer_medium" style="text-align: right;"><?php echo $coverinfo; ?></td>
 					</tr>
 					<tr>
 						<td colspan="2" id="title_small">Alternativtitel</td>
 						<td colspan="1"><?php echo $row["OtherTitles"] != "" ? $row["OtherTitles"] : "- / -"; ?></td>
-						<td rowspan="9">
+						<td rowspan="12" style="vertical-align:top;">
 							<table style="margin-right: 0;">
 								<tr>
 									<td style="width: 40px;">&nbsp;</td>
@@ -126,25 +126,25 @@
 						<td colspan="1"><?php echo $row["File"]; ?></td>
 					</tr>
 					<tr>
-						<td colspan="4" id="spacer_large" style="text-align: right; vertical-align: text-top;"><?php echo $coverinfo; ?></td>
+						<td colspan="3" id="spacer_large"></td>
 					</tr>
 					<?php if ($row["Director"] != "") { ?>
 					<tr>
 						<td colspan="1" id="title_small">von</td>
-						<td colspan="3"><?php echo $row["Director"]; ?></td>
+						<td colspan="2"><?php echo $row["Director"]; ?></td>
 					</tr>
 					<?php } ?>
 					<?php if ($row["Actors"] != "") { ?>
 					<tr>
 						<td colspan="1" id="title_small">mit</td>
-						<td colspan="3"><?php echo $row["Actors"]; ?></td>
+						<td colspan="2"><?php echo $row["Actors"]; ?></td>
 					</tr>
 					<?php } ?>
 					<tr>
-						<td colspan="4" id="spacer_large"></td>
+						<td colspan="3" id="spacer_large"></td>
 					</tr>
 					<tr>
-						<td colspan="4" id="title_small">Bewertung<br>
+						<td colspan="3" id="title_small">Bewertung<br>
 							<table style="margin-left: 0;">
 								<tr>
 									<td colspan="5" id="spacer_small"></td>
